439fc38292e975a18a63125428f9955036c08aa4
seto <y.seto@bizrefine.co.jp>
2021-08-10 13:38:03 +0900

    作業ログの表示内容を変更しています。

--- resources/js/template/Worklog.vue
+++ resources/js/template/Worklog.vue
@@ -1,7 +1,7 @@
 <template>
 　　<div class="work-log">
       <b-table striped hover :items="items" caption-top>
-        <template #table-caption>Work Log.</template>
+        <template #table-caption>作業ログ</template>
       </b-table>
     </div>
 </template>
@@ -12,10 +12,12 @@
     data() {
       return {
         items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+          { 日時: '2021-07-25 14:54:30', ユーザー名: '山田太郎', アクション: '目標設定を差し戻しました' },
+          { 日時: '2021-07-25 16:27:10', ユーザー名: '山田太郎', アクション: '目標設定を承認しました' },
+          { 日時: '2021-07-25 17:49:21', ユーザー名: '山田太郎', アクション: '中間成果を登録しました' },
+          { 日時: '2021-07-25 17:54:18', ユーザー名: '山田太郎', アクション: '中間アドバイスをしました' },
+          { 日時: '2021-07-25 18:04:04', ユーザー名: '中村太郎', アクション: '成果登録の期限を設定しました' },
+          { 日時: '2021-07-25 18:19:46', ユーザー名: '中村太郎', アクション: '一次評価を登録しました' }
         ]
       }
     }
