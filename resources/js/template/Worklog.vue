c5f46d3ff295b2c326715416fab95192bd1811d7
honda_takahiro <t.honda@bizrefine.co.jp>
2021-08-11 09:39:20 +0900

    mod:worklog adjust

--- resources/js/template/Worklog.vue
+++ resources/js/template/Worklog.vue
@@ -2,9 +2,9 @@
 　　<div class="work-log">
       <b-table striped hover outlined :items="items" :fields="fields" caption-top>
         <template #table-caption>作業ログ</template>
-        <template #cell(更新)="row">
+        <template #cell(isNew)="row">
           <div>
-            <b-badge variant="primary">NEW</b-badge>
+            <b-badge variant="primary" v-if="row.value">NEW</b-badge>
           </div>
         </template>
       </b-table>
@@ -16,14 +16,14 @@
     name: 'worklog',
     data() {
       return {
-        fields: ['日時', 'ユーザー名', 'アクション', '更新'],
+        fields: [{ key: 'isNew', label: '' },{ key: 'date', label: '日時' }, { key: 'name', label: '名前' }, { key: 'action', label: 'アクション' }],
         items: [
-          { 日時: '2021-07-25 14:54:30', ユーザー名: '山田太郎', アクション: '目標設定を差し戻しました'},
-          { 日時: '2021-07-25 16:27:10', ユーザー名: '山田太郎', アクション: '目標設定を承認しました'},
-          { 日時: '2021-07-25 17:49:21', ユーザー名: '山田太郎', アクション: '中間成果を登録しました'},
-          { 日時: '2021-07-25 17:54:18', ユーザー名: '山田太郎', アクション: '中間アドバイスをしました'},
-          { 日時: '2021-07-25 18:04:04', ユーザー名: '中村太郎', アクション: '成果登録の期限を設定しました'},
-          { 日時: '2021-07-25 18:19:46', ユーザー名: '中村太郎', アクション: '一次評価を登録しました'}
+          { date: '2021-07-25 14:54:30', name: '山田太郎', action: '目標設定を差し戻しました',isNew:false},
+          { date: '2021-07-25 16:27:10', name: '山田太郎', action: '目標設定を承認しました',isNew:false},
+          { date: '2021-07-25 17:49:21', name: '山田太郎', action: '中間成果を登録しました',isNew:false},
+          { date: '2021-07-25 17:54:18', name: '山田太郎', action: '中間アドバイスをしました',isNew:false},
+          { date: '2021-07-25 18:04:04', name: '中村太郎', action: '成果登録の期限を設定しました',isNew:false},
+          { date: '2021-07-25 18:19:46', name: '中村太郎', action: '一次評価を登録しました',isNew:true}
         ]
       }
     }
