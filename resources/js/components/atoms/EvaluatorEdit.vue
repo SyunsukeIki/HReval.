8615b265b7b0a8b4d6945db36844236503ca83a7
Matsuo <n.matsuo@bizrefine.co.jp>
2021-10-12 15:18:47 +0900

    目標シート配布画面

---
+++ resources/js/components/atoms/EvaluatorEdit.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="memo">
+        <b-modal :id=evaluator_edit_modal_id title="評価者編集" hide-footer size="lg">
+            <div class="start">
+                <div class="badge-space"><b-badge variant="info"><span class="badge-size">ユーザー名</span></b-badge></div>
+                <h4>阿部太郎</h4>
+            </div>
+            <b-badge variant="info"><span class="badge-size">ユーザーリスト</span></b-badge>
+            <template>
+            <div class="list">
+                    <b-col cols="5">
+                        <div id="scrollspy-nested" style="position:relative; height:350px; overflow-y:auto">
+                        <b-form-checkbox><h4 class="item">阿部太郎</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item">阿部太郎</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item">山田花子</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >伊藤純一</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >松尾奈々</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >中村信二</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >田中勇気</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >阿部太郎</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >阿部太郎</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >山田花子</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >浜本千賀子</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >重松千穂美</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >福岡次郎</h4></b-form-checkbox>
+                        <b-form-checkbox><h4 class="item" >福岡次郎</h4></b-form-checkbox>
+                        </div>
+                    </b-col>
+                    <div class="direction">
+                        <div class="button1">
+                            <a href="#"><b-icon icon="arrow-right-square-fill" font-scale=2.5 variant="danger"></b-icon></a>
+                        </div>
+                        <div class="button2">
+                            <a href="#"><b-icon icon="arrow-left-square-fill" font-scale=2.5 variant="primary"></b-icon></a>
+                        </div>
+                    </div>
+                        <b-col cols="5" class="card">
+                                <h4 class="item">①伊藤純一</h4>
+                                <h4 class="item">②山田花子</h4>
+                        </b-col>
+            </div>
+            </template>
+            <!-- 登録ボタンの右寄せ -->
+            <b-container class="bv-example-row">
+                <b-row align-h="end">
+                    <b-col cols="2"><b-button class="mt-1" block @click="$bvModal.hide('bv-modal-example')" variant="primary">保存</b-button></b-col>
+                </b-row>
+            </b-container>
+        </b-modal>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'evaluator-edit',
+        props:{
+            evaluator_edit_modal_id:null,
+            memo_item:null,
+            memo_item_detail:null,
+        },
+        data() {
+            return {
+                fields: [{ key: 'name', label: '投稿者' },{ key: 'memo', label: 'メモ' },{ key: 'date', label: '投稿日' }],
+                items: [
+                { name: '山田太郎', memo: 'ここにメモを表示', date: '20xx年xx月xx日' },
+                { name: '山田太郎', memo: 'ここにメモを表示', date: '20xx年xx月xx日' }
+                ],
+                text:"",
+            }
+        }
+    };
+</script>
+
+
+<style>
+.start{
+    display: flex;
+}
+.badge-space{
+    margin-right: 10px;
+}
+.list{
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+}
+.item{
+    font-size: 20px;
+}
+.card{
+    display: flex;
+    padding-top: 5px;
+}
+.direction{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.button1 :hover{
+    color: rgb(158, 51, 51);
+}
+.button2 :hover{
+    color: rgb(18, 18, 145);
+}
+    
+
+</style>
--- resources/js/components/pages/TemplateDistribution.vue
+++ resources/js/components/pages/TemplateDistribution.vue
@@ -1,16 +1,125 @@
 <template>
         <article>
             <h1 class="title">目標シートテンプレート<br>配布</h1>
+            <div class="sec-content">
+                <div class="section1">         
+                    <b-badge variant="info" ><span class="badge-size">テンプレート名</span></b-badge>
+                    <div class="item1">
+                        <b-form-select v-model="selected" :options="options"></b-form-select>
+                    </div>
+                </div>
+                <div class="section1">
+                    <b-badge variant="info" ><span>配布対象期間</span></b-badge>
+                    <div class="item1">
+                        <b-form-select v-model="selected2" :options="options2"></b-form-select>
+                    </div>
+                </div>
+                <div  class="section2">
+                    <b-badge variant="info" ><span class="badge-size">配布先</span></b-badge>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="ユーザー名、グループ名を入力して絞り込み">
+                        <div class="item2">
+                            <b-form-select v-model="selected3" :options="options3"></b-form-select>
+                        </div>
+                    </div>                                        
+                </div>
+                <div class="end">
+                    <a href="#" class="link-primary">絞り込み結果を全選択</a>
+                </div>
+                <div class="sheetlist-sec1">
+                    <div class="sheetlist-table">
+                        <b-table :items="items" :fields="fields">
+                            <template #cell(evaluator)="">
+                                <div>
+                                    <b-badge pill variant="success"><span class="badge-size2">リーダー</span></b-badge>
+                                </div>
+                            </template>     
+                        </b-table>
+                    </div>
+                </div>
+                <b-button variant="outline-info" v-b-modal.evaluator_edit><b-icon icon="journal-text" aria-hidden="true"></b-icon> 編集</b-button>
+                <evaluator-edit evaluator_edit_modal_id="evaluator_edit" />
+            </div>
         </article>
 </template>
-
 <script>
+  import EvaluatorEdit  from '../atoms/EvaluatorEdit.vue';
   export default {
+
     name: 'template-distribution',
+    components: {EvaluatorEdit},
+    data() {
+            return{   
+                fields: [
+                { key: 'employee', label: 'ユーザー名' },
+                { key: 'evaluator', label: '評価者' },
+    
+                { key: 'group', label: '付属グループ' },
+                { key: 'distributed', label: '配布' }
+                ],
+                items: [
+                { employee: '阿部太郎', evaluator: '<b-badge variant="info">一般社員</b-badge>', group: 'プログラマ（社外事業）', distributed: '済' },
+                { employee: '阿部太郎', evaluator: 'システム事業部', group: 'プログラマ（社外事業）', distributed: '未' },
+                { employee: '鵜飼太郎', evaluator: 'システム事業部', group: '営業', distributed: '未' },
+                { employee: '江田山太郎', evaluator: 'システム事業部', group: '営業', distributed: '済' },
+                { employee: '小野太郎', evaluator: '制作事業部', group: 'デザイナー（社内事業）', distributed: '済' }
+                ], 
+                selected: null,
+                options: [
+                    { value: null, text: 'テンプレートを選択' },
+                    { value: '1', text: '一般社員_2020' },
+                    { value: '2', text: '一般社員_2021' },
+                    { value: '3', text: '営業社員_2020' },
+                    { value: '4', text: '営業社員_2021' },
+                    { value: '5', text: 'リーダー用_2020' },
+                    { value: '6', text: 'リーダー用_2021' }
+                    ],
 
-  }
+                selected2: null,
+                options2: [
+                    { value: null, text: '配布対象期間を選択' },
+                    { value: '1', text: '2020/01/02~2020/06/01' },
+                    { value: '2', text: '2020/06/02~2021/01/01' },
+
+                    { value: '3', text: '2021/01/02~2021/06/01' },
+                    { value: '4', text: '2021/06/02~2022/01/01' },
+                    { value: '5', text: '2022/01/02~2022/06/01' },
+                    { value: '6', text: '2022/06/02~2023/01/01' }
+                    ],
+
+                selected3: null,
+                options3: [
+                    { value: null, text: '配布' },
+                    { value: '1', text: '済' },
+                    { value: '2', text: '未' },
+                    ]
+        }  
+        
+    },
+
+}
 </script>
-
 <style>
+.section1{
+    display: flex;
+    justify-content: flex-start;
+    margin-top: 20px;
+}
+.item1{
+    margin-left: 5px;
+    width:70%;
+}
+.section2{
+    margin-top: 20px;
+}
+.badge-size2{
+    display: flex;
+    justify-content: center;
+}
+.end{
+    display: flex;
+    justify-content: flex-end;
+}
+
 
 </style>
